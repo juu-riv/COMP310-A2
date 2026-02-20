660dd32157e6a007bfc84f2af12a9e9cc77ddb52
juu-riv <juliane.rivard@hotmail.com>
2026-02-20 11:18:54 -0500

    makefile

--- src/scheduler.c
+++ src/scheduler.c
@@ -5,6 +5,7 @@
 #include "queue.h"
 #include "shellmemory.h"
 #include "interpreter.h"
+#include "shell.h"
 
 static enum policy_enum policy = FCFS;
 static int quantum = 2;
@@ -27,29 +28,11 @@ int FCFS_scheduler() {
         struct PCB_struct *pcb = queue_dequeue();
         while (pcb->pc < pcb->start + pcb->length) {
             char *line = mem_get_from(pcb->pc);
-            errCode = scheduler_to_interpreter(line);
+            errCode = parseInput(line); // which calls interpreter()
             pcb->pc++;
         }
         mem_free(pcb);
         PCB_free(pcb);
     }
-    return errCode;
-}
-
-int scheduler_to_interpreter(char *line) {
-    int errCode = 0;
-    char *copy = strdup(line);
-
-    char *argv[MAX_ARGS];
-    int argc = 0;
-
-    char *token = strtok(copy, " ");
-    while (token != NULL && argc < MAX_ARGS) {
-        argv[argc++] = token;
-        token = strtok(NULL, " ");
-    }
-
-    errCode = interpreter(argv, argc);
-    free(copy);
     return errCode;
 }
--- src/scheduler.h
+++ src/scheduler.h
@@ -10,6 +10,4 @@ void set_policy(enum policy_enum scheduling_policy);
 int scheduler();
 int FCFS_scheduler();
 
-int scheduler_to_interpreter(char *line);
-
 #endif //A2_261120458_SCHEDULER_H
